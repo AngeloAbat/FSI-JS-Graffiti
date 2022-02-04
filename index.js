fc922908470e947eb284ac62193b76b204496a1f
Angelo <angeloabat25@gmail.com>
2022-02-03 19:20:50 -0800

    Maturnt

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+let dogImages = document.querySelectorAll(".dog-image")
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.transform = "scaleY(-1) scaleX(-1)"
+}
+
+
+let dogName = document.querySelectorAll(".dog-name")
+for(let x = 0; x < dogName.length; x++){
+    dogName[x].style.textAlign = "left"
+}
+
+let footer = document.querySelector(".footer")
+footer.style.color = "green"
+
